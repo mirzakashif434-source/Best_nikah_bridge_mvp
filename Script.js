35799c491c65c4b8440de3044d18f5fc95635b8d
mirzakashif434-source <mirzakashif434@gmail.com>
2026-08-18 14:46:47 +0300

    Create Script.js
    function startProfile() {
      alert("Welcome to Best Nikah Bridge! Profile creation will be added next.");
    }
    
    function signIn() {
      alert("Sign In will be connected in the next step.");
    }
    
    function viewProfile() {
      alert("Profile details will be available after verification.");
    }
    
    function showProfiles() {
      document.getElementById("profiles").scrollIntoView({
        behavior: "smooth"
      });
    }

---
+++ Script.js
@@ -0,0 +1,17 @@
+function startProfile() {
+  alert("Welcome to Best Nikah Bridge! Profile creation will be added next.");
+}
+
+function signIn() {
+  alert("Sign In will be connected in the next step.");
+}
+
+function viewProfile() {
+  alert("Profile details will be available after verification.");
+}
+
+function showProfiles() {
+  document.getElementById("profiles").scrollIntoView({
+    behavior: "smooth"
+  });
+}
