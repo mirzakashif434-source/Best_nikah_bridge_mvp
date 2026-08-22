842a98351e57cfaf0c1e35d7a990afd9c1ab79bc
mirzakashif434-source <mirzakashif434@gmail.com>
2026-08-22 13:24:32 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,3 +1,12 @@
-package com.nikahbridge.app;
-import android.app.Activity; import android.os.Bundle; import android.webkit.WebSettings; import android.webkit.WebView;
-public class MainActivity extends Activity { @Override protected void onCreate(Bundle b){super.onCreate(b); WebView w=new WebView(this); WebSettings s=w.getSettings(); s.setJavaScriptEnabled(true); s.setDomStorageEnabled(true); w.loadUrl("file:///android_asset/index.html"); setContentView(w);} }
+package com.nikahbridge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.activity_list_item);
+    }
+}
