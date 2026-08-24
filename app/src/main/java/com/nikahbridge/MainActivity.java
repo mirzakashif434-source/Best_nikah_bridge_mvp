790de7abd496d39c57fc3e57acf734efc0a0406d
mirzakashif434-source <mirzakashif434@gmail.com>
2026-08-24 16:21:11 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/nikahbridge/MainActivity.java
@@ -0,0 +1,24 @@
+package com.nikahbridge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView screen = new TextView(this);
+        screen.setText("Best Nikah Bridge\n\nTrusted • Simple • Safe");
+        screen.setTextSize(24);
+        screen.setTextColor(Color.BLACK);
+        screen.setGravity(Gravity.CENTER);
+        screen.setPadding(40, 40, 40, 40);
+
+        setContentView(screen);
+    }
+}
