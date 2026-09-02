4b318b0aa8d06437075c04f3423e414957106c0f
mirzakashif434-source <mirzakashif434@gmail.com>
2026-09-03 00:50:17 +0300

    Register additive Help Line admin functions

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,5 +1,6 @@
 // Additive functions entrypoint: preserve every existing export from index.js.
 const existing = require('./index');
 const helpLine = require('./helpLineAI');
-Object.assign(existing, helpLine);
+const helpLineAdmin = require('./helpLineAdmin');
+Object.assign(existing, helpLine, helpLineAdmin);
 module.exports = existing;
