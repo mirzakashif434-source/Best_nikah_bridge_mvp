e6429e73aa88275cbc85de4bb9af9c4b560162ec
mirzakashif434-source <mirzakashif434@gmail.com>
2026-09-21 23:48:16 +0300

    Step 3: remove remaining unused Firebase Community imports

--- app/src/main/java/com/nikahbridge/CommunityChatActivity.java
+++ app/src/main/java/com/nikahbridge/CommunityChatActivity.java
@@ -19,8 +19,6 @@ import android.os.Handler;
 import android.os.Looper;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.Query;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
