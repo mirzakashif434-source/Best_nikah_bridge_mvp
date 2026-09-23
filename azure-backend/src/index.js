37e77471b37812961ba0c191ab5f8acf8d5dea45
mirzakashif434-source <mirzakashif434@gmail.com>
2026-09-23 11:27:25 +0300

    Register Azure settings APIs

--- azure-backend/src/index.js
+++ azure-backend/src/index.js
@@ -64,3 +64,5 @@ require("./helpLine");
 // Firebase migration #5: additive Azure owner earnings/dashboard and settlement APIs.
 require("./ownerEarnings");
 require("./firebaseParity");
+
+require("./settings");
