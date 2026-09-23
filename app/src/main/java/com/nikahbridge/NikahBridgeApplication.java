d778e867680ad97a39c6ce2e2ae19140a381e31f
mirzakashif434-source <mirzakashif434@gmail.com>
2026-09-23 20:33:28 +0300

    Fix UI audit compile imports

--- app/src/main/java/com/nikahbridge/NikahBridgeApplication.java
+++ app/src/main/java/com/nikahbridge/NikahBridgeApplication.java
@@ -10,6 +10,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import com.google.android.gms.ads.MobileAds;
